1d256127a880e1deb3e9a2dfe4ae8d5014fbaf4c
Brian Sierra <basierraferrer@gmail.com>
2024-11-26 22:04:24 -0500

    Updated scraper method to get episode info

--- utils/timer.ts
+++ utils/timer.ts
@@ -1,12 +1,12 @@
 import {PLAYER} from './constants';
 import * as cheerio from 'cheerio';
 
-export const getTimerSelector = (playerName: string)=>{
-    let selector = '';
+export const getTimerSelector = (playerName: string) => {
+  let selector = '';
 
-  switch(playerName){
+  switch (playerName) {
     case PLAYER.MEGA:
-      break;
+      throw new Error(`Selector no definido para ${playerName}`);
     case PLAYER.SW:
       selector = '.jw-slider-time';
       break;
@@ -14,35 +14,39 @@ export const getTimerSelector = (playerName: string)=>{
       selector = '.jw-text-duration';
       break;
     case PLAYER.FEMBED:
-      selector = '';
-      break;
-    default: 
-     break;       
+      throw new Error(`Selector no definido para ${playerName}`);
+    default:
+      throw new Error(`Selector no definido para ${playerName}`);
   }
   return selector;
 };
 
-export const getTimerArray = (textHTML:string, title: string) => {
-    let timerArray:string[] = [];
-  
-    if (title === PLAYER.SW) {
-      const $cheerio = cheerio.load(textHTML);
-      const timerSelector = getTimerSelector(title);
-      timerArray = $cheerio(timerSelector).attr('aria-valuetext')?.replace('0 seconds of ', '').trim().split(',') as string[];
-    }
-  
-    if (title === PLAYER.YOUR_UPLOAD) {
-      timerArray = textHTML.split(':');
-      timerArray[0] += ' minutes';
-      timerArray[1] += ' seconds';
-    }
-  
-    return timerArray;
-  };
+export const getTimerArray = (textHTML: string, title: string) => {
+  let timerArray: string[] = [];
+
+  if (title === PLAYER.SW) {
+    const $cheerio = cheerio.load(textHTML);
+    const timerSelector = getTimerSelector(title);
+    timerArray = $cheerio(timerSelector)
+      .attr('aria-valuetext')
+      ?.replace('0 seconds of ', '')
+      .trim()
+      .split(',') as string[];
+  }
+
+  if (title === PLAYER.YOUR_UPLOAD) {
+    timerArray = textHTML.split(':');
+    timerArray[0] += ' minutes';
+    timerArray[1] += ' seconds';
+  }
+
+  return timerArray;
+};
 
 export const getTimerMilliseconds = (textHTML: string, title: string) => {
-    const timerArray = getTimerArray(textHTML, title);
-    const totalMilliseconds:number = timerArray.reduce((total:number, item: string) => {
+  const timerArray = getTimerArray(textHTML, title);
+  const totalMilliseconds: number = timerArray.reduce(
+    (total: number, item: string) => {
       const [value, unit] = item.split(' ');
       let milliseconds = 0;
       if (unit === 'minutes') {
@@ -51,6 +55,8 @@ export const getTimerMilliseconds = (textHTML: string, title: string) => {
         milliseconds = parseInt(value, 10) * 1000;
       }
       return total + milliseconds;
-    }, 0);
-    return totalMilliseconds;
-  };
+    },
+    0,
+  );
+  return totalMilliseconds;
+};
